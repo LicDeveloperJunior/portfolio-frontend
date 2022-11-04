4c4e9dc0ba3564c25b588f7d4a65aecf4464e12d
LicDeveloperJunior <agustincv1997@gmail.com>
2022-11-04 08:23:26 -0300

    Se creo el servicio de login con autenticacion de firebase

---
+++ src/app/servicios/login/login.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { Auth, signInWithEmailAndPassword, signOut } from '@angular/fire/auth';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoginService {
+
+  constructor(private auth: Auth) { }
+
+  login({email, contrasenia}:any) {
+    return signInWithEmailAndPassword(this.auth, email, contrasenia);
+  }
+
+  logout() {
+    return signOut(this.auth);
+  }
+
+  estaLogeado() {
+    return this.auth.currentUser;
+  }
+}
